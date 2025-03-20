3260aeffa66760b5da9b012a9e42b16a364eb376
RandomSearch <101704343+RandomSearch18@users.noreply.github.com>
2025-03-20 23:09:30 +0000

    It works now

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from pypdf import PdfReader, PdfWriter
 # 30 pages is the maximum that PaperCut will allow in a single print job
 # Note that this should be even so that double-sided printing can carry forward
 PAGES_PER_CHUNK = 10
+TEMP_FOLDER = Path(".", "temp")
 
 
 def get_file_path():
@@ -27,23 +28,30 @@ def main():
     total_pages = len(reader.pages)
     print(f"Processing {total_pages} pages")
 
+    # Prepare the temp folder
+    TEMP_FOLDER.mkdir(exist_ok=True, parents=True)
+    for file in TEMP_FOLDER.glob("*"):
+        file.unlink()
+
     done = False
     chunk_number = 1
+    pages = list(reader.pages)
     while not done:
         print(f"Creating chunk #{chunk_number}...")
         writer = PdfWriter()
         pages_added = 0
-        for page in reader.pages:
+        while pages_added < PAGES_PER_CHUNK:
+            if len(pages) == 0:
+                break
+            page = pages.pop(0)
             writer.add_page(page)
             pages_added += 1
-            if pages_added == PAGES_PER_CHUNK:
-                break
         # Write the file
-        output_path = Path("temp", f"Part {chunk_number} - {file_path.stem}.pdf")
+        output_path = TEMP_FOLDER / f"Part {chunk_number} - {file_path.stem}.pdf"
         output_path.parent.mkdir(exist_ok=True)
         with open(output_path, "wb") as output_file:
             writer.write(output_file)
-        print(f"Created {output_path}")
+        print(f"Wrote {output_path.name}")
         # End the loop if we've gotten through the PDF
         chunk_number += 1
         if pages_added < PAGES_PER_CHUNK:
